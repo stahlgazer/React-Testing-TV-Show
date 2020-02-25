a43952a88baaae72b2c695a5e88289a1ca432ae5
gavin stahl <stahlgavin@gmail.com>
2020-02-24 23:42:02 -0800

    test created but not functional

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import {
   render,
   fireEvent,
   waitForElement,
-  wait,
+  wait
 } from "@testing-library/react";
 import { fetchShow as mockFetchShow } from "./api/fetchShow";
 import App from "./App";
@@ -11,19 +11,21 @@ import App from "./App";
 jest.mock("./api/fetchShow");
 console.log(mockFetchShow);
 
+const episodes = {
+  season1: [{ number: 1 }, { number: 2 }, { number: 3 },{number: 4},{number: 5},{number: 6},{number: 7},{number: 8},]
+};
+
 // use async/await
 test("App fetches Show data and render data", async () => {
-  mockFetchShow.mockResolvedValueOnce();
+  mockFetchShow.mockResolvedValueOnce(episodes);
 
   const { getByText, queryAllByTestId, getByTestId } = render(<App />);
 
   const button = getByText(/select a season/i);
   fireEvent.click(button);
 
-//   getByText(/we are fetching data/i);
-//   await wait(() => {
-//     getByTestId("mission-list");
-//   });
-//   expect(queryAllByTestId("mission")).toHaveLength(2);
-// });
-});
+    await wait(() => {
+      getByTestId("episodes");
+    });
+    expect(queryAllByTestId("episodes")).toHaveLength(8);
+  });
